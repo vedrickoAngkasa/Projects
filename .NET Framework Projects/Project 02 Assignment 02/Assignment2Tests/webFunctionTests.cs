51901bd4180ec5ede7a35ec7a636f1c4c2a7a0db
vedricko <vedrickoangkasa19@gmail.com>
2023-08-17 01:54:32 +0800

    .NET Framework Files

---
+++ .NET Framework Projects/Project 02 Assignment 02/Assignment2Tests/webFunctionTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Tests
+{
+    [TestClass()]
+    public class webFunctionTests
+    {
+
+        [TestMethod()]
+        public void testAdapterClasss()
+        {
+
+            adapter testing = new webFunction();
+            testing.setName("Adam Test");
+            Console.WriteLine("Name: "  + testing.getName());
+        }
+
+
+            [TestMethod()]
+        public void testGetterAndSetter()
+        {
+
+            adapter testing = new webFunction();
+            testing.setName("Adam Test");
+            testing.setLat("-32");
+            testing.setLng("144");
+            testing.setTypeSupport("Volunteer");
+            testing.setSupportLevel("Full Time");
+            testing.setImages("someone.jpg");
+
+            Console.WriteLine("Details: " + testing.getName() + testing.getLat() + testing.getLng() + testing.getTypeSupport() + testing.getSupportLevel() + testing.getImages());
+
+
+
+        }
+
+
+
+
+
+    }
+}
